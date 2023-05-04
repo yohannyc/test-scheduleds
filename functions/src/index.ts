2df8411e78ab6fee7dc1d2d1f64395eb29053481
yohannyc <yohanny@celebreak.eu>
2023-05-04 13:23:14 -0500

    removed scheduled function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,24 +6,7 @@ admin.initializeApp();
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-const eventMaxAgeMs = 180000;
-export const citiesPerformance = functions
-    .runWith({ timeoutSeconds: 120, failurePolicy: true })
-    .pubsub.schedule('*/10  * * * *')
-    .onRun((event) => {
-        console.log("Hola mundo", eventMaxAgeMs);
-        if (new Date().getTime() > Date.parse(event.timestamp) + eventMaxAgeMs) {
-            console.log(`Dropping eventsSummary execution`);
-            return;
-        }
-        citiesPerformanceFn();
-    });
-
-
-const citiesPerformanceFn = () => {
-    throw new Error("Error");
-};
+export const helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+});
